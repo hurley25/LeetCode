c227ab344264cecd544119b31bec78ae65154497
hurley25 <liuhuan1992@gmail.com>
2016-01-31 18:34:33 +0800

    add Rotate_Array
    Signed-off-by: hurley25 <liuhuan1992@gmail.com>

---
+++ src/Rotate_Array.cpp
@@ -0,0 +1,38 @@
+/*
+ * Rotate an array of n elements to the right by k steps.
+ *
+ * For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4]
+ *
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using std::vector;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        size_t len = nums.size();
+        vector<int> tmps = nums;
+        for (int i = 0; i < len; ++i) {
+           nums[(i+k) % len] = tmps[i];
+        }
+    }
+};
+
+int main()
+{
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
+    Solution solution;
+    solution.rotate(nums, 3);
+
+    for (auto i : nums) {
+        std::cout << i << " ";
+    }
+    std::cout << "\n";
+
+    return 0;
+}
+
